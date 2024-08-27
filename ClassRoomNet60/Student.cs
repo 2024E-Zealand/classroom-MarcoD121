08f3a364fb399c4750e7bb0cd13425981f2d5798
Marco Dybig <marcodybig@hotmail.com>
2024-08-27 13:43:34 +0200

    Constructed and properties created

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public Student(string name, int birth_Month, int birthday)
+        {
+            Name = name;
+            Birth_Month = birth_Month;
+            Birthday = birthday;
+        }
+
+        public string Name { get; set; }
+        public int Birth_Month { get; set; }
+        public int Birthday { get; set; }
+
+    }
+
+    
+}
